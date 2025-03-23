b326352e9ab0f64647523d284cfb479b5ddd629c
Vinz <macmini@192.168.1.2>
2025-03-24 01:59:15 +0530

    fixed lottie path type

--- components/Lottie/LottieIcon1.tsx
+++ components/Lottie/LottieIcon1.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import Lottie from "react-lottie-player"
-import lottieJson from "../../public/lottie/wired-outline-212-arrow-1-rounded-hover-pinch.json"
+import lottieJson from "../../public/Lottie/lottie-animation-01.json"
 
 const LottieIcon1: React.FC<{ width?: number; height?: number }> = ({ width = 64, height = 64 }) => {
   return <Lottie loop animationData={lottieJson} play style={{ width, height }} />
