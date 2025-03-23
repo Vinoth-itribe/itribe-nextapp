81fabc6fe7620520bde64cddf47d1d9da272dc4c
Vinz <macmini@192.168.1.2>
2025-03-24 01:48:22 +0530

    fixed lottie error

--- components/Lottie/LottieIcon1.tsx
+++ components/Lottie/LottieIcon1.tsx
@@ -2,14 +2,10 @@ import type React from "react"
 import Lottie from "react-lottie-player"
 import lottieJson from "../../public/lottie/wired-outline-212-arrow-1-rounded-hover-pinch.json"
 
-interface LottieIconProps {
-  width?: number
-  height?: number
-}
-
-const LottieIcon: React.FC<LottieIconProps> = ({ width = 64, height = 64 }) => {
+const LottieIcon1: React.FC<{ width?: number; height?: number }> = ({ width = 64, height = 64 }) => {
   return <Lottie loop animationData={lottieJson} play style={{ width, height }} />
 }
 
-export default LottieIcon
+
+export default LottieIcon1
 
