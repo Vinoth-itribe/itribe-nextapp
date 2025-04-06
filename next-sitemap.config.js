388024c64c20e259d252678bae2a5636021f61ce
Vinz <macmini@192.168.1.7>
2025-04-06 11:59:22 +0530

    removed sitemap generation

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://inboundtribe.com', // 🔁 Replace with your actual domain
-    generateRobotsTxt: true,
-    generateIndexSitemap: true,
-    sourceDir: 'app', // 👈 This tells it to look in the app/ folder
-  };
-  
